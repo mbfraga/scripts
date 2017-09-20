55a24c831b851a6b2f9f26d6e04db1ff5bfc288f
Martin Fraga <mbfraga@gmail.com>
2017-09-20 01:55:26 -0700

    Minor changes
    * fixed colorsterm.
    * renamed a few of the scripts to remove the extension.
    * moved a few zsh functions to scripts on here.

--- colorsterm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Show all 16 terminal colors
-
-for i in {0..7};do
-   printf "\033[0;3%dm  %d NORM █████████████ 0;3%dm \033[0m\n" "$i" "$i" "$i"
-done
-for i in {0..7};do
-   printf "\033[1;3%dm 1%d NORM █████████████ 1;3%dm \033[0m\n" "$i" "$i" "$i"
-done
